e1060c8765cb6686eb4be2e51c4065171eb6392b
Shinigami <denshaotoko@outlook.com>
2017-10-19 18:51:02 +0200

    Initial commit

---
+++ app/src/main/java/asteroides/example/org/tabs/Tab2.java
@@ -0,0 +1,34 @@
+package asteroides.example.org.tabs;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Vespertino on 19/10/2017.
+ */
+
+public class Tab2 extends Fragment {
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.tab2, container, false);
+
+    }
+
+}
